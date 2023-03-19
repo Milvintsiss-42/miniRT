282cda74ec0930c03a6964d435718d7868089723
Milvintsiss <milvintsiss@gmail.com>
2023-03-19 14:35:24 +0100

    Updates custom error messages

--- includes/common.h
+++ includes/common.h
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 19:05:09 by ple-stra          #+#    #+#             */
-/*   Updated: 2023/03/19 14:30:29 by ple-stra         ###   ########.fr       */
+/*   Updated: 2023/03/19 14:41:34 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@
 
 # define ERR_UNKNOWN			"Unknown error"
 
-# define ERR_WRG_NB_ARG			"Error: Incorrect number of arguments"
-# define ERR_INV_EXT			"Error: Invalid file extension"
-# define ERR_INV_FILE			"Error: Invalid file"
+# define ERR_WRG_NB_ARG			"Incorrect number of arguments"
+# define ERR_INV_EXT			"Invalid file extension"
+# define ERR_INV_FILE			"Invalid file"
 
-# define ERR_FAILED_INIT_MLX	"Error: Failed to init mlx"
-# define ERR_FAILED_OP_WIN_MLX	"Error: Failed to open mlx window"
-# define ERR_MLXCRASH			"Error: MLX crashed"
+# define ERR_FAILED_INIT_MLX	"MiniLibX: Failed to init mlx"
+# define ERR_FAILED_OP_WIN_MLX	"MiniLibX: Failed to open mlx window"
+# define ERR_MLXCRASH			"MiniLibX: MLX crashed"
 
 # define KEY_ECHAP		0xff1b
 
@@ -47,7 +47,8 @@ typedef struct s_mrt {
 void		exit_mrt(t_mrt mrt, int status);
 
 int			ft_perror(t_mrt mrt, const char *error_str);
-int			ft_ctxtperror(t_mrt mrt, const char *filename, const char *error_str);
+int			ft_ctxtperror(t_mrt mrt, const char *filename,
+				const char *error_str);
 int			ft_perror_errno(t_mrt mrt);
 int			ft_ctxtperror_errno(t_mrt mrt, const char *filename);
 

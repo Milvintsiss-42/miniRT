165ce09330a1e66af1faf9013266c66984bff22f
Milvintsiss <milvintsiss@gmail.com>
2023-04-04 21:59:03 +0200

    Shortened parse_normalized_vector method by using parse_vector method

--- srcs/parsing/parsing_helpers_vectors.c
+++ srcs/parsing/parsing_helpers_vectors.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 17:29:21 by ple-stra          #+#    #+#             */
-/*   Updated: 2023/03/23 17:38:38 by ple-stra         ###   ########.fr       */
+/*   Updated: 2023/04/04 21:58:41 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,21 +34,8 @@ bool	parse_vector(char *str, t_vec3 *vector_ret)
 
 bool	parse_normalized_vector(char *str, t_vec3 *vector_ret)
 {
-	char	**s_str;
-
-	s_str = ft_split(str, ',');
-	if (!s_str)
+	if (!parse_vector(str, vector_ret))
 		return (false);
-	if (ft_splitlen(s_str) != 3 || !is_double(s_str[0])
-		|| !is_double(s_str[1]) || !is_double(s_str[2]))
-	{
-		ft_freesplit(s_str);
-		return (false);
-	}
-	vector_ret->x = ft_atof(s_str[0]);
-	vector_ret->y = ft_atof(s_str[1]);
-	vector_ret->z = ft_atof(s_str[2]);
-	ft_freesplit(s_str);
 	if (vector_ret->x < -1 || vector_ret->x > 1
 		|| vector_ret->y < -1 || vector_ret->y > 1
 		|| vector_ret->z < -1 || vector_ret->z > 1)

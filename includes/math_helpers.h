c8161f549553fba713f745da47dc8b3d3fd073d0
Milvintsiss <milvintsiss@gmail.com>
2023-04-27 17:06:04 +0200

    Adds t_quadratic to use less variables when computing quadratic equations

--- includes/math_helpers.h
+++ includes/math_helpers.h
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 17:43:48 by ple-stra          #+#    #+#             */
-/*   Updated: 2023/04/08 17:45:06 by ple-stra         ###   ########.fr       */
+/*   Updated: 2023/04/27 17:05:50 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,14 @@
 # ifndef M_PI
 #  define M_PI 3.14159265358979323846
 # endif
+
+typedef struct s_quadratic {
+	double	a;
+	double	b;
+	double	c;
+	double	discriminant;
+	double	t1;
+	double	t2;
+}	t_quadratic;
 
 #endif

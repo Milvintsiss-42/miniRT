c81db043a16f25d19966083f8a9443f5e7897a75
Milvintsiss <milvintsiss@gmail.com>
2023-10-30 00:30:34 +0100

    Shows test_card_f while loading first frame

--- srcs/mlx_helpers/open_window.c
+++ srcs/mlx_helpers/open_window.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 14:25:57 by ple-stra          #+#    #+#             */
-/*   Updated: 2023/10/23 02:12:23 by ple-stra         ###   ########.fr       */
+/*   Updated: 2023/10/30 00:16:40 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,13 +45,15 @@ static void	setup_mlx_window(t_mrt *mrt, double width_fraction,
 			PROGRAM_NAME);
 	if (!mlx->window)
 		exit_mrt(*mrt, ft_perror(*mrt, ERR_FAILED_OP_WIN_MLX));
+	create_new_img(mrt);
+	draw_test_card_f(mrt);
+	set_new_img_to_screen(mrt);
 }
 
 void	open_window(t_mrt *mrt, double width_fraction, double height_fraction)
 {
 	init_mlx_lib(mrt);
 	setup_mlx_window(mrt, width_fraction, height_fraction);
-	reframe(mrt);
 	mlx_key_hook(mrt->mlx.window, on_keypressed, mrt);
 	mlx_mouse_hook(mrt->mlx.window, on_mouse_event, mrt);
 	mlx_hook(mrt->mlx.window, ON_DESTROY, 0, on_window_cross_clicked, mrt);
